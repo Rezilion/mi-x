e9f0376cfbc0b1342522f62206f5a7feac6996e3
OfriOuzan <ofriouzan@gmail.com>
2022-06-21 20:00:37 +0300

    Pylint changes to os_type.py

--- Modules/os_type.py
+++ Modules/os_type.py
@@ -1,3 +1,6 @@
+"""
+Support for modules which written for avoiding repetitive code.
+"""
 from Modules import run_command, constants
 
 BASIC_COLOR = '\033[00m'
@@ -7,17 +10,16 @@ NEGATIVE_RESULT = '\033[91m'
 QUESTION = '\033[94m'
 
 
-# This function checks if the operation system is Linux.
 def linux(debug, container_name):
+    """This function checks if the operation system is Linux."""
     os_type = 'uname -s'
     print(constants.FULL_QUESTION_MESSAGE.format('Is it Linux?'))
     pipe_os_type = run_command.command_output(os_type, debug, container_name)
     os_type_output = pipe_os_type.stdout
-    if os_type_output.__contains__('Linux'):
+    if 'Linux' in os_type_output:
         print(constants.FULL_NEGATIVE_RESULT_MESSAGE)
         print(constants.FULL_EXPLANATION_MESSAGE.format('You are running on a Linux operation system'))
         return True
-    else:
-        print(constants.FULL_POSITIVE_RESULT_MESSAGE)
-        print(constants.FULL_EXPLANATION_MESSAGE.format('You are not running on a Linux operation system'))
-        return False
+    print(constants.FULL_POSITIVE_RESULT_MESSAGE)
+    print(constants.FULL_EXPLANATION_MESSAGE.format('You are not running on a Linux operation system'))
+    return False

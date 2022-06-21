16a1d955f3d1d3ceffcc0cc97d31e162afac47a1
OfriOuzan <ofriouzan@gmail.com>
2022-06-21 19:58:37 +0300

    Pylint changes to kernel_version.py

--- Modules/kernel_version.py
+++ Modules/kernel_version.py
@@ -1,5 +1,8 @@
-from Modules import run_command, commons, constants
+"""
+Support for semver and other modules which written for avoiding repetitive code.
+"""
 import semver
+from Modules import run_command, commons, constants
 
 BASIC_COLOR = '\033[00m'
 EXPLANATION = '\033[90m'
@@ -8,36 +11,34 @@ POSITIVE_RESULT = '\033[92m'
 QUESTION = '\033[94m'
 
 
-# This function checks if the host kernel is vulnerable according to the fixed kernel release.
 def check_kernel(min_kernel_version, max_kernel_version, debug):
+    """This function checks if the host kernel is vulnerable according to the fixed kernel release."""
     vulnerable = False
     kernel_version_command = 'uname -r'
     print(constants.FULL_QUESTION_MESSAGE.format('Is kernel version affected?'))
     pipe_kernel_version = run_command.command_output(kernel_version_command, debug, container_name=False)
     kernel_version = pipe_kernel_version.stdout
     valid_kernel_version = commons.valid_kernel_version(kernel_version)
-    if valid_kernel_version:
-        if semver.compare(max_kernel_version, valid_kernel_version) == 1 and \
-                semver.compare(min_kernel_version, valid_kernel_version) == -1:
-            vulnerable = True
-            print(f'{NEGATIVE_RESULT}Yes{BASIC_COLOR}')
-        else:
-            print(f'{POSITIVE_RESULT}No{BASIC_COLOR}')
-        print(f'{EXPLANATION}According to your os release, vulnerable kernel versions are between: {min_kernel_version}'
-              f' to {max_kernel_version}')
-        print(f'Your kernel version: {valid_kernel_version[:constants.END]}{BASIC_COLOR}')
-    else:
+    if not valid_kernel_version:
         print(f'{EXPLANATION}Kernel version unsupported value{BASIC_COLOR}')
         return 'Unsupported'
+    if semver.compare(max_kernel_version, valid_kernel_version) == 1 and \
+            semver.compare(min_kernel_version, valid_kernel_version) == -1:
+        vulnerable = True
+        print(f'{NEGATIVE_RESULT}Yes{BASIC_COLOR}')
+    else:
+        print(f'{POSITIVE_RESULT}No{BASIC_COLOR}')
+    print(f'{EXPLANATION}According to your os release, vulnerable kernel versions are between: {min_kernel_version}'
+          f' to {max_kernel_version}')
+    print(f'Your kernel version: {valid_kernel_version[:constants.END]}{BASIC_COLOR}')
     return vulnerable
 
 
-# This function returns the kernel version.
 def get_kernel_version(debug):
+    """This function returns the kernel version."""
     kernel_version_command = 'uname -r'
     pipe_kernel_version = run_command.command_output(kernel_version_command, debug, container_name=False)
     kernel_version = pipe_kernel_version.stdout
     if kernel_version:
         return kernel_version
-    else:
-        return False
+    return False
